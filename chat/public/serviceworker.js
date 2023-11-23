8ee45755323224d37e366ddda3dd9420c9802722
achyut vardhan <achyutvardhan234@gmail.com>
2023-11-23 16:18:04 +0530

    done

--- chat/public/serviceworker.js
+++ chat/public/serviceworker.js
@@ -26,7 +26,7 @@ self.addEventListener('fetch', (event) => {
         caches.match(event.request)
             .then(() => {
                 return fetch(event.request) 
-                    .catch(() => caches.match('offline.html'))
+                    .catch(() => caches.match('index.html'))
             })
     )
 });
